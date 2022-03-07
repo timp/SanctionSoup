f7e9b79d69c38a3c660a1d9b565e63eb2126141f
timp <timp@paneris.org>
2022-03-07 22:22:50 +0000

    Download, and record download date, to show when nothing changes

---
+++ get.sh
@@ -0,0 +1,5 @@
+curl -k -H "Accept-Charset: utf-8;q=0.7,*;q=0.7"   https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1058962/UK_Sanctions_List.xml > UK_Sanctions_List.xml
+
+echo date >> gets.txt
+
+
