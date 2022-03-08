6db72802dd9314f632f7e663f36d152d4e32fa57
timp <timp@paneris.org>
2022-03-08 22:36:47 +0000

    don't echo

--- get.sh
+++ get.sh
@@ -1,5 +1,5 @@
 curl -L -k -H "Accept-Charset: utf-8;q=0.7,*;q=0.7"   https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1058962/UK_Sanctions_List.xml > UK_Sanctions_List.xml
 
-echo date >> gets.txt
+date >> gets.txt
 
 
